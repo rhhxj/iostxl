dee4227065a97b88c70efb42899550cc19325cf0
admin <admin@chu1e1adeimac.local>
2016-07-13 15:30:18 +0800

    Initial Commit

---
+++ iostxl/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iostxl
+//
+//  Created by admin on 16/7/13.
+//  Copyright © 2016年 20141105047. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
